4a0951c7f62835c6d678d6720ae72a6e464844d7
Eman Hassan <emanhhsacr@gmail.com>
2022-05-10 04:08:58 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+var push = document.querySelector("#push");
+var inp = document.querySelector('input');
+var tasks = document.querySelector('#tasks')
+push.addEventListener('click' , addtask)
+function addtask(){
+    if (inp.value == "") alert("please enter a task")
+    tasks.innerHTML += 
+    `<div class="task">
+    <span id ="taskname">
+     ${inp.value} 
+     </span>
+     <button class = "delete">
+     <i class="fas fa-trash-alt"></i> 
+      </button>
+     </div>`;
+    inp.value = "";
+    
+var deleteitem = document.querySelectorAll(".delete");
+
+for ( var i = 0; i< deleteitem.length ; i++){
+    deleteitem[i].onclick = function(){
+        this.parentElement.remove();
+        }
+    }
+    var tasksf = document.querySelectorAll(".task");
+    for(var i =0; i< tasksf.length; i++){
+        tasksf[i].onclick = function(){
+            this.classList.toggle('completed')
+        }
+    }
+}
+
